77a94ddff50b18ef54404531aadbd9a53a0add82
christopher nafrere <nafrere.christopher@gmail.com>
2022-06-16 19:47:28 +0200

    connect react to node

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
+app.use(express.static("view/build"));
+
 app.use(express.json());
 
 app.use(router);
+
 
 app.listen(PORT, console.log(`Lancement du server sur le port ${PORT}`));
